74ebd7177a85d3ee92e9383b50be8cd76a5dc3ef
maksym_solo <solmaks05@gmail.com>
2026-03-30 16:34:52 +0200

    new custom model was trained and quatized based on colorless data from the radar sensor

--- stm32ai-modelzoo-services/image_classification/tf/src/models/custom_model.py
+++ stm32ai-modelzoo-services/image_classification/tf/src/models/custom_model.py
@@ -1,59 +1,64 @@
-# /*---------------------------------------------------------------------------------------------
-#  * Copyright (c) 2022 STMicroelectronics.
-#  * All rights reserved.
-#  *
-#  * This software is licensed under terms that can be found in the LICENSE file in
-#  * the root directory of this software component.
-#  * If no LICENSE file comes with this software, it is provided AS-IS.
-#  *--------------------------------------------------------------------------------------------*/
-
 from typing import Tuple, Optional
 import keras
 from keras import layers
 
 
-def get_custom_model(num_classes: int = None, input_shape: Tuple[int, int, int] = None,
-                     dropout: Optional[float] = None, pretrained: bool = False, **kwargs) -> keras.Model:
+def get_custom_model(
+    num_classes: int = None,
+    input_shape: Tuple[int, int, int] = None,
+    dropout: Optional[float] = None,
+    pretrained: bool = False,
+    **kwargs
+) -> keras.Model:
     """
-    Creates a custom image classification model with the given number of classes and input shape.
+    Lightweight custom image classification model for embedded deployment.
 
-    Args:
-        num_classes (int): Number of classes in the classification task.
-        input_shape (Tuple[int, int, int]): Shape of the input image.
-        dropout (Optional[float]): Dropout rate to be applied to the model.
-
-    Returns:
-        keras.Model: Custom image classification model.
+    Intended for grayscale or sensor-like inputs such as (96, 64, 1).
     """
+
     if pretrained:
-      print("WARNING: No pretrained weights are found for 'custom' model. Random weights are used instead.")
-    
-    # Define the input layer
-    inputs = keras.Input(shape=input_shape)
+        print("WARNING: No pretrained weights are available for 'custom_model'. Random weights will be used.")
 
-    # Define the feature extraction layers
-    x = layers.Conv2D(16, (3, 3), strides=(1, 1), padding='same', use_bias=False)(inputs)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-    x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(32, (3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-    x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same', use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-    x = layers.MaxPooling2D()(x)
+    if input_shape is None:
+        raise ValueError("`input_shape` must be provided.")
+    if num_classes is None:
+        raise ValueError("`num_classes` must be provided.")
 
-    # Define the classification layers
-    x = layers.GlobalAveragePooling2D()(x)
-    if dropout:
-        x = layers.Dropout(dropout)(x)
-    if num_classes > 2:
-        outputs = layers.Dense(num_classes, activation="softmax")(x)
+    inputs = keras.Input(shape=input_shape, name="input")
+
+    # Block 1
+    x = layers.Conv2D(16, kernel_size=3, padding="same", use_bias=False, name="conv1")(inputs)
+    x = layers.BatchNormalization(name="bn1")(x)
+    x = layers.ReLU(name="relu1")(x)
+    x = layers.MaxPooling2D(pool_size=2, name="pool1")(x)
+
+    # Block 2
+    x = layers.Conv2D(32, kernel_size=3, padding="same", use_bias=False, name="conv2")(x)
+    x = layers.BatchNormalization(name="bn2")(x)
+    x = layers.ReLU(name="relu2")(x)
+    x = layers.MaxPooling2D(pool_size=2, name="pool2")(x)
+
+    # Block 3
+    x = layers.Conv2D(64, kernel_size=3, padding="same", use_bias=False, name="conv3")(x)
+    x = layers.BatchNormalization(name="bn3")(x)
+    x = layers.ReLU(name="relu3")(x)
+    x = layers.MaxPooling2D(pool_size=2, name="pool3")(x)
+
+    # Optional extra capacity, still lightweight
+    x = layers.Conv2D(64, kernel_size=3, padding="same", use_bias=False, name="conv4")(x)
+    x = layers.BatchNormalization(name="bn4")(x)
+    x = layers.ReLU(name="relu4")(x)
+
+    # Classifier head
+    x = layers.GlobalAveragePooling2D(name="gap")(x)
+
+    if dropout is not None and dropout > 0:
+        x = layers.Dropout(dropout, name="dropout")(x)
+
+    if num_classes == 2:
+        outputs = layers.Dense(1, activation="sigmoid", name="classifier")(x)
     else:
-        outputs = layers.Dense(1, activation="sigmoid")(x)
+        outputs = layers.Dense(num_classes, activation="softmax", name="classifier")(x)
 
-    # Define and return the model
     model = keras.Model(inputs=inputs, outputs=outputs, name="custom_model")
     return model
